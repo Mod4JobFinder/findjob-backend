3bb772393a62608329f5fefef8137e312e8013b5
Andrew Johnston <av.johnst@gmail.com>
2021-05-15 15:03:00 -0600

    Add user update path and request spec
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates_length_of :password, minimum: 8
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    has_many :saved_jobs, dependent: :destroy
-    validates :first_name, :last_name, :city, :state, :zipcode, :password_confirmation, presence: true
-end
+  has_secure_password
+  validates_length_of :password, minimum: 8, :if => :password
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, :last_name, :city, :state, :zipcode, presence: true
+  validates :password_confirmation, presence: true, :if => :password
+  has_many :saved_jobs, dependent: :destroy
+end 
---
+++ spec/requests/api/v1/users/users_patch_request_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users Update", type: :request do
+  before :each do 
+    @user = create(:user,
+                  email: "whatever@example.com",
+                  password: "password1",
+                  password_confirmation: "password1",
+                  first_name: "Khoa",
+                  last_name: "Nguyen",
+                  city: "The Moon",
+                  state: "MN",
+                  zipcode: '80000'
+                )
+  end 
+
+  describe 'happy path' do 
+    it 'should return a 200 if the users email is present and the attributes are valid' do 
+      valid_body = {
+                      "email": "whatever@example.com",
+                      "first_name": "Andrew",
+                      "last_name": "Johnston",
+                      "city": "Denver",
+                      "state": "CO",
+                      "zipcode": "80020"
+                    }
+
+      patch api_v1_users_path, params: valid_body
+      request.headers['Content-Type'] = 'application/json'
+      request.headers['Accept'] = 'application/json'
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(200)
+      expect(json[:data]).to eq('user successfully updated')
+      
+      # require 'pry';binding.pry
+      # expect(@user.first_name).to eq('Andrew')
+      # expect(@user.last_name).to eq('Johnston')
+      # expect(@user.city).to eq('Denver')
+      # expect(@user.state).to eq('CO')
+      # expect(@user.zipcode).to eq('80020')
+    end
+  end 
+
+  describe 'sad path' do 
+    it 'should return 400 if user cant be found' do 
+      valid_body = {
+                      "email": "whatever123@example.com",
+                      "first_name": "Andrew",
+                      "last_name": "Johnston",
+                      "city": "Denver",
+                      "state": "CO",
+                      "zipcode": "80020"
+                    }
+      
+      patch api_v1_users_path, params: valid_body
+      request.headers['Content-Type'] = 'application/json'
+      request.headers['Accept'] = 'application/json'
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(400)
+      expect(json[:error]).to eq('invalid parameters')
+    end 
+  end 
+end 
