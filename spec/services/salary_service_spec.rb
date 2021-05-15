50f5a3ba39cdfd3aea6fa32adc647c7c10b3dbdc
Andrew Johnston <av.johnst@gmail.com>
2021-05-15 10:57:56 -0600

    Change salary endpoint to the micro service
    Co-authored-by: Joseph Budina <52386927+josephbudina@users.noreply.github.com>
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>

--- spec/services/salary_service_spec.rb
+++ spec/services/salary_service_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe SalaryService, type: :model do
   describe 'class methods' do 
-    describe '#get_salary_link' do 
-      it 'should return the link to a citys salary endpoint', :vcr do 
-        response = SalaryService.get_salary_link('denver')
+    # describe '#get_salary_link' do 
+    #   it 'should return the link to a citys salary endpoint', :vcr do 
+    #     response = SalaryService.get_salary_link('denver')
 
-        expect(response).to eq('https://api.teleport.org/api/urban_areas/slug:denver/salaries/')
-      end
+    #     expect(response).to eq('https://api.teleport.org/api/urban_areas/slug:denver/salaries/')
+    #   end
       
-      it 'should return [] if city is not an urban area', :vcr do
-        response = SalaryService.get_salary_link('Littleton')
+    #   it 'should return [] if city is not an urban area', :vcr do
+    #     response = SalaryService.get_salary_link('Littleton')
 
-        expect(response).to eq([])
-      end
-    end
+    #     expect(response).to eq([])
+    #   end
+    # end
 
     describe '#get_salary_info', :vcr do 
       it 'should return job salaries for a city', :vcr do 
@@ -27,11 +27,11 @@ RSpec.describe SalaryService, type: :model do
         expect(response[0][:max]).to be_a(String)
       end
       
-      it 'should return [] if city is not an urban area', :vcr do 
-        response = SalaryService.get_salary_link('Mesa')
+      # it 'should return [] if city is not an urban area', :vcr do 
+      #   response = SalaryService.get_salary_link('Mesa')
 
-        expect(response).to eq([])
-      end
-    end 
+      #   expect(response).to eq([])
+      # end
+    end
   end 
 end
