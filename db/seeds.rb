59ee6bcff5bee91876ee5c692a092a096668a21c
Andrew Johnston <av.johnst@gmail.com>
2021-05-18 16:46:41 -0600

    Add destroy all job listings to seed file for heroku
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 SalaryFacade.flood_db
+JobListing.destroy_all
