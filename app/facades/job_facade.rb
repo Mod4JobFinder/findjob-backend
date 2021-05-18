2dbe3901e6caf68690ed9cdb253ddcc75ae42b3b
Andrew Johnston <av.johnst@gmail.com>
2021-05-18 16:40:38 -0600

    Add guard to category when hitting the microservice
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>

--- app/facades/job_facade.rb
+++ app/facades/job_facade.rb
@@ -2,13 +2,21 @@ class JobFacade
   def self.create_job_listings(city, title = nil)
     listings = JobService.get_job_listings(city, title)
     listings.map do |listing|
+      if !listing[:attributes][:category]
+        category = 'none'
+      elsif listing[:attributes][:category].class == String
+        category = listing[:attributes][:category]
+      else
+        category = listing[:attributes][:category].join(', ')
+      end
+
       hash = { 
         title: listing[:attributes][:title],
         company: listing[:attributes][:company],
         url: listing[:attributes][:link],
         description: listing[:attributes][:description].gsub("**BREAK**", "\n"),
         location: listing[:attributes][:location],
-        category: listing[:attributes][:category].join(', '),
+        category: category,
         date: Date.parse(listing[:attributes][:publish_date]).strftime('%a %B %e, %Y').gsub('  ', ' '),
         search_title: title,
         search_city: city
